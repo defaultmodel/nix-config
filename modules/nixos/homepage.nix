497d21e9740649e92115faa6d5ac117ffa9ee5e5
Hilan Meyran <hmeyran@amossys.fr>
2025-05-05 13:42:06 +0000

    added some info to homepage

--- modules/nixos/homepage.nix
+++ modules/nixos/homepage.nix
@@ -6,7 +6,9 @@ let
   cfg = config.def.homepage;
   srv = config.services.homepage-dashboard;
   certloc = "/var/lib/acme/defaultmodel.eu.org";
-in {
+  url = "home.defaultmodel.eu.org";
+in
+{
   options.def.homepage = { enable = mkEnableOption "Homepage"; };
 
   config = mkIf cfg.enable {
@@ -31,8 +33,60 @@ in {
           };
         };
       };
+      bookmarks = [
+        {
+          Dev = [
+            {
+              Github = [
+                {
+                  abbr = "GH";
+                  href = "https://github.com/";
+                  icon = "github.png";
+                }
+              ];
+            }
+          ];
+        }
+        {
+          Entertainment = [
+            {
+              YouTube = [
+                {
+                  abbr = "YT";
+                  href = "https://youtube.com/";
+                  icon = "reddit.png";
+                }
+              ];
+              Reddit = [
+                {
+                  abbr = "YT";
+                  href = "https://reddit.com/";
+                  icon = "reddit.png";
+                }
+              ];
+            }
+          ];
+        }
+      ];
+      widgets = [
+        {
+          resources = {
+            cpu = true;
+            memory = true;
+            uptime = true;
+            network = true;
+          };
+        }
+        {
+          search = {
+            provider = "duckduckgo";
+            showSearchSuggestions = true;
+          };
+        }
+      ];
     };
 
+    ### REVERSE PROXY ###
     services.caddy = {
       virtualHosts."home.defaultmodel.eu.org".extraConfig = ''
         reverse_proxy http://localhost:${toString srv.port}
@@ -41,5 +95,10 @@ in {
         }
       '';
     };
+
+    services.adguardhome.settings.dns.rewrites = [{
+      domain = url;
+      answer = config.networking.interfaces.ens18.ipv4;
+    }] ++ (config.services.adguardhome.settings.dns.rewrites or [ ]);
   };
 }
