f22ca0f9ac292a3ff4acd1de72e541e7e19eb6e7
Hilan Meyran <hilanmeyran@protonmail.com>
2025-06-01 00:06:48 +0200

    switch to deluge for labels support

--- hosts/server/local/rhodes/services/torrent.nix
+++ hosts/server/local/rhodes/services/torrent.nix
@@ -1,7 +1,6 @@
-{ config, pkgs, lib, ... }:
-with lib;
+{ config, ... }:
 let
-  srv = config.services.transmission;
+  srv = config.services.deluge;
   certloc = "/var/lib/acme/defaultmodel.eu.org";
   url = "torrent.defaultmodel.eu.org";
 
@@ -25,81 +24,44 @@ in {
     owner = srv.user;
   };
 
-  # Enable and specify VPN namespace to confine service in.
-  systemd.services.transmission.vpnConfinement = {
-    enable = true;
-    vpnNamespace = "wg";
-  };
-
-  # Port mappings
-  vpnNamespaces.wg = {
-    portMappings = [{
-      from = srv.settings.rpc-port;
-      to = srv.settings.rpc-port;
-      protocol = "both";
-    }];
-    openVPNPorts = [{
-      port = srv.settings.peer-port;
-      protocol = "both";
-    }];
-  };
-
   users.users.torrent = {
     isSystemUser = true;
     group = "media";
   };
 
-  services.transmission = {
+  services.deluge = {
     enable = true;
     user = "torrent";
     group = "media";
 
-    webHome = pkgs.flood-for-transmission;
-    credentialsFile = config.age.secrets.torrent-credentials.path;
+    authFile = config.age.secrets.torrent-credentials.path;
+    web.enable = true;
 
-    openRPCPort = true;
-    openPeerPorts = true;
+    declarative = true;
+    config = {
+      download_location = downloadDir;
+      enabled_plugins = [ "Label" "WebUi" ];
+      # max_download_speed = 20000;
+      # max_upload_speed = 10000;
 
-    settings = {
-      download-dir = downloadDir;
-      incomplete-dir-enabled = true;
-      incomplete-dir = incompleteDir;
-      watch-dir-enabled = true;
-      watch-dir = watchDir;
-
-      rpc-bind-address =
-        "0.0.0.0"; # Bind RPC/WebUI to VPN network namespace address
-      rpc-port = 9091;
-      rpc-whitelist-enabled = true;
-      # "192.168.15.5"; Access from default network namespace
-      # "192.168.1.*";  Access from other machines on specific subnet
-      # "127.0.0.1";    Access through loopback within VPN network namespace
-      rpc-whitelist =
-        strings.concatStringsSep "," ([ "127.0.0.1,192.168.1.*,192.168.15.5" ]);
-      rpc-host-whitelist-enabled = true;
-      rpc-host-whitelist = url;
-      rpc-authentication-required = false;
-
-      blocklist-enabled = true;
-      blocklist-url =
-        "https://github.com/Naunter/BT_BlockLists/raw/master/bt_blocklists.gz";
-
-      peer-port = 50000;
-      dht-enabled = true;
-      pex-enabled = true;
-      utp-enabled = false;
-      encryption = 1;
-      port-forwarding-enabled = false;
-
-      anti-brute-force-enabled = true;
-      anti-brute-force-threshold = 10;
+      random_outgoing_ports = false;
+      pre_allocate_storage = true;
+      # Networking
+      allow_remote = true;
+      daemon_port = 58846;
+      random_port = false;
+      listen_ports = [ 6881 ];
+      enc_level = 1; # Full stream encryption
+      # Features
+      upnp = false;
+      natpmp = false;
     };
   };
 
   ### REVERSE PROXY ###
   services.caddy = {
     virtualHosts.${url}.extraConfig = ''
-      reverse_proxy http://192.168.15.1:${toString srv.settings.rpc-port}
+      reverse_proxy http://192.168.15.1:${toString srv.web.port}
       tls ${certloc}/cert.pem ${certloc}/key.pem {
         protocols tls1.3
       }
@@ -114,9 +76,38 @@ in {
   }];
 
   ### HOMEPAGE ###
-  def.homepage.categories."Downloaders"."Transmission" = {
-    icon = "transmission.png";
+  def.homepage.categories."Downloaders"."Deluge" = {
+    icon = "deluge.png";
     description = "Torrent downloader";
     href = "https://${url}";
+  };
+
+  ### VPN ###
+  # Enable and specify VPN namespace to confine service in.
+  systemd.services.deluged.vpnConfinement = {
+    enable = true;
+    vpnNamespace = "wg";
+  };
+  systemd.services.delugeweb.vpnConfinement = {
+    enable = true;
+    vpnNamespace = "wg";
+  };
+
+  # Port mappings
+  vpnNamespaces.wg = {
+    portMappings = [{
+      from = srv.web.port;
+      to = srv.web.port;
+    }];
+    openVPNPorts = [
+      {
+        port = srv.config.daemon_port;
+        protocol = "both";
+      }
+      {
+        port = 6881;
+        protocol = "both";
+      }
+    ];
   };
 }
