9b3a85e3647c99580493bfdc07a1be69fab66466
Hilan Meyran <hilanmeyran@protonmail.com>
2025-07-10 19:44:56 +0200

    kde native applications

--- hosts/desktop/wolfcall/configuration.nix
+++ hosts/desktop/wolfcall/configuration.nix
@@ -46,6 +46,16 @@
     wayland-utils # Wayland utilities
     wl-clipboard # Command-line copy/paste utilities for Wayland
 
+    ### KDE Applications
+    kdePackages.discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
+    kdePackages.kcalc # Calculator
+    kdePackages.kcharselect # Tool to select and copy special characters from all installed fonts
+    kdePackages.kcolorchooser # A small utility to select a color
+    kdePackages.kolourpaint # Easy-to-use paint program
+    kdePackages.ksystemlog # KDE SystemLog Application
+    kdePackages.sddm-kcm # Configuration module for SDDM
+    kdePackages.isoimagewriter # Optional: Program to write hybrid ISO files onto USB disks
+    kdePackages.partitionmanager # Optional Manage the disk devices, partitions and file systems on your computer
   ];
 
   programs.fish = {
