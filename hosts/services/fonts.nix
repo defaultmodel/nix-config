8dce2b8421f88a8feed67963dd5f8a8475cf6233
Hilan Meyran <hilanmeyran@protonmail.com>
2025-08-05 22:47:56 +0200

    add atkinson-hyperlegible font

--- hosts/services/fonts.nix
+++ hosts/services/fonts.nix
@@ -1,16 +1,17 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     nerd-fonts.ubuntu
     nerd-fonts.ubuntu-sans
     noto-fonts-color-emoji
     nerd-fonts.ubuntu-mono
+    atkinson-hyperlegible-next
+    atkinson-hyperlegible-mono
   ];
 
   fonts.fontconfig = {
     enable = true;
     defaultFonts = {
-      serif = [ "nerd-font-ubuntu" ];
+      serif = [ "atkinson-hyperlegible" ];
       sansSerif = [ "nerf-font-ubuntu-sans" ];
       emoji = [ "noto-fonts-color-emoji" ];
       monospace = [ "nerd-fonts-ubuntu-mono" ];
