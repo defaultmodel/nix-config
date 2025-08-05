3f7d0098da3c1487af7035f9f83f864228fa3659
Hilan Meyran <hilanmeyran@protonmail.com>
2025-08-05 22:51:55 +0200

    enable docker support

--- hosts/desktop/wolfcall/configuration.nix
+++ hosts/desktop/wolfcall/configuration.nix
@@ -85,6 +85,7 @@
 
   virtualisation = {
     containers.enable = true;
+    docker.enable = true;
     podman = {
       enable = true;
       # Required for containers under podman-compose to be able to talk to each other.
