26d7097bd4834afecb20c598830da1586583e2f5
Hilan Meyran <hilanmeyran@protonmail.com>
2025-08-05 22:52:04 +0200

    Enable LFS support for git

--- hosts/desktop/wolfcall/home/git.nix
+++ hosts/desktop/wolfcall/home/git.nix
@@ -3,6 +3,7 @@
     enable = true;
     userName = "Hilan Meyran";
     userEmail = "hilanmeyran@protonmail.com";
+    lfs.enable = true;
     extraConfig = {
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
