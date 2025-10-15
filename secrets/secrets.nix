35c66d475bee5be9379706e12815b06f9d70f521
Hilan Meyran <hilanmeyran@protonmail.com>
2025-10-15 09:25:55 +0200

    secrets rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,27 +1,36 @@
 let
-  root-wolfcall =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJwMavP3djIhEVnNJjBUoMOIVlBI1bjyxg3NNtDpP/Op";
   defaultmodel-wolfcall =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAtsgdnGkeAWcGjsLyQRhCJDJyfwlD0euUW37u8ou6px";
   root-rhodes =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAO/RjuSJEP2Ne0sr4q8PQdXUoGuZFTZYtvY/7JsJVan";
+  root-lemnos =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILzfJLEwBnRwDBQlSG28krTAPp1LImWxnNm3LfkcFlcF";
+  root-agios =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK5typsS1z9XxIBmDOxk803w/bs7ijbr2WnmzZbdcGcw";
 in {
   "wg-conf.age".publicKeys = [ defaultmodel-wolfcall root-rhodes ];
   "paperless-admin-password.age".publicKeys =
     [ defaultmodel-wolfcall root-rhodes ];
   "slskd-credentials.age".publicKeys = [ defaultmodel-wolfcall root-rhodes ];
   "torrent-credentials.age".publicKeys = [ defaultmodel-wolfcall root-rhodes ];
-  "dns-provider-api-key.age".publicKeys = [ defaultmodel-wolfcall root-rhodes ];
-  "rss-credentials.age".publicKeys = [ defaultmodel-wolfcall root-rhodes ];
+  "dns-provider-api-key.age".publicKeys =
+    [ defaultmodel-wolfcall root-rhodes root-agios ];
+  "rss-credentials.age".publicKeys = [ defaultmodel-wolfcall root-agios ];
   "bazarr-api-key.age".publicKeys = [ defaultmodel-wolfcall root-rhodes ];
   "radarr-api-key.age".publicKeys = [ defaultmodel-wolfcall root-rhodes ];
   "sonarr-api-key.age".publicKeys = [ defaultmodel-wolfcall root-rhodes ];
   "lidarr-api-key.age".publicKeys = [ defaultmodel-wolfcall root-rhodes ];
   "vaultwarden-admin-token.age".publicKeys =
-    [ defaultmodel-wolfcall root-rhodes ];
-  "hetzner-backup-passphrase.age".publicKeys =
-    [ defaultmodel-wolfcall root-rhodes ];
-  "radicale-credentials.age".publicKeys = [ defaultmodel-wolfcall root-rhodes ];
+    [ defaultmodel-wolfcall root-agios ];
+  "radicale-credentials.age".publicKeys = [ defaultmodel-wolfcall root-agios ];
   "radarr-api-key-plain.age".publicKeys = [ defaultmodel-wolfcall root-rhodes ];
   "sonarr-api-key-plain.age".publicKeys = [ defaultmodel-wolfcall root-rhodes ];
+
+  "hetzner-backup-passphrase.age".publicKeys =
+    [ defaultmodel-wolfcall root-rhodes ];
+  "hetzner-backup-agios-passphrase.age".publicKeys =
+    [ defaultmodel-wolfcall root-agios ];
+  "authelia-main-storageEncryptionKey".publicKeys =
+    [ defaultmodel-wolfcall root-lemnos ];
+  "authelia-main-jwtSecret".publicKeys = [ defaultmodel-wolfcall root-lemnos ];
 }
