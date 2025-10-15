88cb88e84528edd5c7a29ab76d65ff6eddb0f37c
Hilan Meyran <hilanmeyran@protonmail.com>
2025-10-15 09:39:17 +0200

    add fail2ban

--- hosts/services/ssh-hardening.nix
+++ hosts/services/ssh-hardening.nix
@@ -1,5 +1,4 @@
-{ ... }:
-{
+{ ... }: {
   services.openssh = {
     allowSFTP = false;
     # https://infosec.mozilla.org/guidelines/openssh#modern-openssh-67
@@ -44,5 +43,25 @@
       MaxSessions 2
       TCPKeepAlive no
     '';
+  };
+
+  services.fail2ban = {
+    enable = true;
+    maxretry = 5;
+    ignoreIP = [
+      # Whitelist RFC 1918
+      "10.0.0.0/8"
+      "172.16.0.0/12"
+      "192.168.0.0/16"
+    ];
+    bantime = "24h"; # Ban IPs for one day on the first ban
+    bantime-increment = {
+      enable = true; # Enable increment of bantime after each violation
+      formula =
+        "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
+      multipliers = "1 2 4 8 16 32 64";
+      maxtime = "168h"; # Do not ban for more than 1 week
+      overalljails = true; # Calculate the bantime based on all the violations
+    };
   };
 }
